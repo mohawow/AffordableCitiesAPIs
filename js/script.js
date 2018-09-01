6249776e09cb60c3e1ea7300fbb6609f07ec5827
Mohammed Mahbashi <mohawow@gmail.com>
2018-08-31 18:38:58 -0700

    added chart js and compared two cities

---
+++ js/script.js
@@ -0,0 +1,111 @@
+const QUANDL_API_URL='https://www.quandl.com/api/v3/datasets/ZILLOW/';
+var months = [
+    'January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September',
+    'October', 'November', 'December'
+];
+function monthNumToName(monthnum) {
+    return months[monthnum - 1] || '';
+}
+$(".submitbutton").on('click',function(e) {
+    var data1set=new Array(),data2set=new Array(),datamonth=new Array(),totalcost1=0,totalcost2=0,cityname1='',cityname2='',median1=0,median2=0;
+    data1set=[0];
+    data2set=[0];
+    var state1=$('#inputState1').val();
+    var state2=$('#inputState2').val();
+    console.log(state1);
+    var request = new XMLHttpRequest();
+    request.open('GET', QUANDL_API_URL+'S'+state1+'_ZRISFRR?api_key=hzmgKTSmvyCcncCbDGtd', false);
+    request.onload = function () {
+        cityname1=jQuery.parseJSON(this.response).dataset.name.toString().split('-')[3];
+        var data1 = jQuery.parseJSON(this.response).dataset.data;
+        $.each(data1, function( index, value ) {
+            var $date1=value.toString().split(',')[0];
+            //console.log( index + ": " + $date1.toString().split('-')[0] );
+            if ($date1.toString().split('-')[0]==2018){
+                datamonth.push(monthNumToName($date1.toString().split('-')[1]));
+                data1set.push(value.toString().split(',')[1]);
+                totalcost1=totalcost1+parseInt(value.toString().split(',')[1]);
+            }
+        });
+        median1=Math.round(totalcost1/(parseInt(data1set.length)-1));
+        console.log(data1set);
+        //console.log(jQuery.parseJSON(this.response).dataset.data);
+    };
+    request.send();
+    var request = new XMLHttpRequest();
+    request.open('GET', QUANDL_API_URL+'S'+state2+'_ZRISFRR?api_key=hzmgKTSmvyCcncCbDGtd', false);
+    request.onload = function () {
+        var data2 = jQuery.parseJSON(this.response).dataset.data;
+        cityname2=jQuery.parseJSON(this.response).dataset.name.toString().split('-')[3];
+        $.each(data2, function( index, value ) {
+            var $date2=value.toString().split(',')[0];
+            //console.log( index + ": " + $date1.toString().split('-')[0] );
+            if ($date2.toString().split('-')[0]==2018){
+                //datamonth.push(monthNumToName($date2.toString().split('-')[1]));
+                data2set.push(value.toString().split(',')[1]);
+                totalcost2=totalcost2+parseInt(value.toString().split(',')[1]);
+            }
+        });
+        median2=Math.round(totalcost2/(parseInt(data2set.length)-1));
+        console.log(totalcost2);
+
+        //console.log(cityname1,cityname2,median1,median2);
+    };
+    request.send();
+    console.log(cityname1,cityname2,median1,median2);
+    var barChartData = {
+        labels: datamonth.reverse(),
+        datasets: [{
+            label: cityname1,
+            backgroundColor: 'rgba(0, 99, 132, 0.6)',
+            yAxisID: 'y-axis-1',
+            data: data1set.reverse()
+        }, {
+            label: cityname2,
+            backgroundColor: 'rgba(100, 99, 132, 0.6)',
+            yAxisID: 'y-axis-2',
+            data: data2set.reverse()
+        }]
+    };
+
+    // Code from chart.js documentation
+    var ctx = document.getElementById('chart').getContext('2d');
+    var myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: cityname1+' VS '+cityname2
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: true
+            },
+            scales: {
+                yAxes: [{
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    id: 'y-axis-1',
+                }, {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    id: 'y-axis-2',
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }],
+            }
+        }
+    });
+// Git prices to show on homeprice
+    $('#homeprice').html(cityname1+':'+' $'+median1+'<br>'+cityname2+':'+' $'+median2);
+    myBar.update();
+    e.preventDefault();
+
+    return false;
+});
